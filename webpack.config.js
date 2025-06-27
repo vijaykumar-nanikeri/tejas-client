9b1b878ef836620bbad1d21a88620eaa2a8576b2
vijaykumar.nanikeri <your-username@users.noreply.github.com>
2025-06-27 19:52:28 +0530

    CS-6: Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/"), // Use 'static' instead of 'contentBase'
+    },
+    compress: true,
+    port: 9000,
+    open: true, // Automatically open the browser
+  },
+};
